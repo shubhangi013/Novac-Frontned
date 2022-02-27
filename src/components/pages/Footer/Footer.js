784b5aeb5ccb6a6e4082bd128996caadd07cd943
S-kus <saumyakus@gmail.com>
2022-02-27 21:30:08 +0530

    updated footer

--- src/components/pages/Footer/Footer.js
+++ src/components/pages/Footer/Footer.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './Footer.css';
-import { Button } from '../../Button';
 import { Link } from 'react-router-dom';
 import {
   FaFacebook,
   FaInstagram,
-  FaYoutube,
   FaTwitter,
   FaLinkedin
 } from 'react-icons/fa';
@@ -13,71 +11,11 @@ import { MdAttachMoney } from 'react-icons/md';
 
 function Footer() {
   return (
-    <div className='footer-container'>
-      <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-          Join our exclusive membership to receive the latest news and trends
-        </p>
-        <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
-        </div>
-      </section>
-      <div className='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
-            <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
-          </div>
-          <div className='footer-link-items'>
-            <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
-          </div>
-        </div>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
-            <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
-          </div>
-          <div className='footer-link-items'>
-            <h2>Social Media</h2>
-            <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link>
-          </div>
-        </div>
-      </div>
-      <section className='social-media'>
-        <div className='social-media-wrap'>
-          <div className='footer-logo'>
-            <Link to='/' className='social-logo'>
-            <MdAttachMoney className='navbar-icon' />
-              NOVAC
-            </Link>
-          </div>
-          <small className='website-rights'>All rights reserved© 2022</small>
-          <div className='social-icons'>
+    <div className='footer'>
+      <div className='social_content'>
+        <MdAttachMoney className='footer_icon' />
+        <p>Join us to get your big-break into Fintech and everything related to finance!</p>
+        <div className='social-icons'>
             <Link
               className='social-icon-link'
               to='/'
@@ -96,16 +34,6 @@ function Footer() {
             </Link>
             <Link
               className='social-icon-link'
-              to={
-                '/'
-              }
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <FaYoutube />
-            </Link>
-            <Link
-              className='social-icon-link'
               to='/'
               target='_blank'
               aria-label='Twitter'
@@ -121,8 +49,30 @@ function Footer() {
               <FaLinkedin />
             </Link>
           </div>
+          <p className='copyright'>© 2022 All rights reserved</p>
+      </div>
+      <div className='pages_links'>
+        <div className='footer-links'>
+          <div className='footer-link-wrapper'>
+            <div className='footer-link-items'>
+              <h2>MINIMAL</h2>
+              <Link to='/'>About Us</Link>
+              <Link to='/'>Contact Us</Link>
+              <Link to='/'>FAQ</Link>
+            </div>
+            <div className='footer-link-items'>
+              <h2>LEGAL</h2>
+              <Link to='/'>Terms and Condition</Link>
+              <Link to='/'>Privacy Policy</Link>
+            </div>
+            <div className='footer-link-items'>
+              <h2>CONTACT</h2>
+              <Link to='mailto:support@novac.com'>support@novac.com</Link>
+              <Link to='/'>IIT (BHU) Varanasi, India</Link>
+            </div>
+          </div>
         </div>
-      </section>
+      </div>
     </div>
   );
 }
